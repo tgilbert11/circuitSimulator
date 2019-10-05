1467cbabbaa8bce754813a7c96093a6d4fc3c9aa
Taylor Gilbert <taylorg353@gmail.com>
2019-10-05 07:37:41 -0700

    started class files

--- circuitSimulator/Pin.swift
+++ circuitSimulator/Pin.swift
@@ -1,10 +1,46 @@
-//
-//  Pin.swift
-//  circuitSimulator
-//
-//  Created by Taylor Gilbert on 9/8/19.
-//  Copyright © 2019 Taylor Gilbert. All rights reserved.
-//
-
-import Foundation
-
+class Pin: CustomStringConvertible {
+    static var pins: [Pin] = []
+    var net: Net?
+    var state: Driving = .impeded
+    var connectedTo: NetValue = .floating
+    var description: String { return "\(self.net == nil ? "*" : "")\(self.state)" }
+    
+    init() {
+        Pin.pins.append(self)
+    }
+    
+    func connectTo(_ pin: Pin) {
+        print("proximal pin: \(Unmanaged.passUnretained(self).toOpaque()); distal pin: \(Unmanaged.passUnretained(pin).toOpaque())")
+        if let definiteDistalNet = pin.net {
+            //print("distal net exists + ", terminator: "")
+            if let definiteProximalNet = self.net {
+                //print("proximal net exists")
+                //kill proximal net, preserve distal net
+                //print("net collision")
+                for proximalPin in definiteProximalNet.pins {
+                    definiteDistalNet.pins.append(proximalPin)
+                    proximalPin.net = definiteDistalNet
+                }
+                self.net = definiteDistalNet
+            }
+            else {
+                //print("proximal net was nil")
+                self.net = definiteDistalNet
+                definiteDistalNet.append(self)
+            }
+        }
+        else {
+            //print("distal net was nil + ", terminator: "")
+            if let definiteProximalNet = self.net {
+                //print("proximal net exists")
+                pin.net = definiteProximalNet
+                definiteProximalNet.append(pin)
+            }
+            else {
+                //print("proximal net was nil")
+                net = Net(name: "noName", pins: self, pin)
+                pin.net = net
+            }
+        }
+    }
+}
